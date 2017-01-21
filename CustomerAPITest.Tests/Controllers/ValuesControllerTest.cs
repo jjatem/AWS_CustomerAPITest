784638e1631a8bc2dfd2ce79f34fdbea9ab5a201
jjatem <jjatem@hotmail.com>
2017-01-20 21:55:30 -0500

    Add project files.

---
+++ CustomerAPITest.Tests/Controllers/ValuesControllerTest.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomerAPITest;
+using CustomerAPITest.Controllers;
+
+namespace CustomerAPITest.Tests.Controllers
+{
+    [TestClass]
+    public class ValuesControllerTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            // Arrange
+            CustomersController controller = new CustomersController();
+
+            // Act
+            IEnumerable<customer> result = controller.Get();
+
+            //Test count
+            int count = result.Count();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(count, result.Count());
+            Assert.AreEqual("Apple Inc.", result.ElementAt(0).customer_name);
+            Assert.AreEqual("Google, Inc.", result.ElementAt(1).customer_name);
+        }
+
+        [TestMethod]
+        public void GetById()
+        {
+            // Arrange
+            CustomersController controller = new CustomersController();
+
+            //Test value
+            customer testValue = new customer()
+            {
+                id = 5,
+                customer_name = "Tesla Motors",
+                city = "Palo Alto",
+                region_state = "CA",
+                date_added = DateTime.Parse("2017-01-15 18:02:58")
+            };
+
+            // Act
+            customer result = controller.Get(5);
+
+            testValue.date_added = result.date_added;
+
+            // Assert
+            Assert.AreEqual(testValue.customer_name, result.customer_name);
+        }
+
+        [TestMethod]
+        public void Post()
+        {
+            // Arrange
+            CustomersController controller = new CustomersController();
+
+            customer NewCustomerTest = new customer()
+            {
+                customer_name = "Test Customer",
+                city = "Test City",
+                date_added = DateTime.Now,
+                region_state = "GA"
+            };
+
+            // Act
+            bool Result = controller.Post(NewCustomerTest);
+
+            // Assert
+            Assert.IsTrue(Result);
+        }
+
+        [TestMethod]
+        public void Put()
+        {
+            // Arrange
+            CustomersController controller = new CustomersController();
+
+            customer UpdateValue = new customer()
+            {
+                id = 37,
+                customer_name = "Tesla Motors Updated Name",
+                date_added = DateTime.Now,
+                city = "Palo Alto UPD",
+                region_state = "CA"
+            };
+
+            // Act
+            controller.Put(37, UpdateValue);
+
+            // Assert
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            // Arrange
+            CustomersController controller = new CustomersController();
+
+            // Act
+            controller.Delete(33);
+
+            // Assert
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ CustomerAPITest/Controllers/CustomersController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CustomerAPITest.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        private CustomerContextModel dbContext = new CustomerContextModel();
+
+        // GET api/values
+        public IEnumerable<customer> Get()
+        {
+            return dbContext.customers.AsEnumerable();            
+        }
+
+        // GET api/values/5
+        public customer Get(int id)
+        {
+            return dbContext.customers.AsEnumerable().Where(x => x.id == id).FirstOrDefault();
+        }
+
+        // POST api/values
+        public bool Post([FromBody]customer value)
+        {
+            int NewId = 0;
+
+            if (value != null &&
+                value is customer)
+            {
+                #region deprecated
+                //deprecated set max id
+                //IEnumerable<customer> customers = dbContext.customers.AsEnumerable();
+
+                //if (customers?.Count() > 0)
+                //{
+                //    NewId = customers.Max(item => item.id) + 1;                             
+                //}
+                //else
+                //{
+                //    NewId++;
+                //}
+                #endregion
+
+                //set id as zero - mysql autoincrement will set the right pk value
+                value.id = NewId;
+                value.date_added = DateTime.Now;
+
+                dbContext.customers.Add(value);
+                dbContext.SaveChanges();
+
+                Ok(true);
+
+                return true;
+
+            }
+
+            return false;
+        }
+
+        // PUT api/values/5
+        public bool Put(int id, [FromBody]customer value)
+        {
+            if (value == null)
+            {
+                BadRequest("Invalid Request");
+                return false;
+            }
+
+            /*
+             * Update customer info
+             */
+            customer FoundMatch = this.Get(id);
+
+            if (FoundMatch != null && FoundMatch.id == id)
+            {
+                dbContext.customers.Remove(FoundMatch);
+                dbContext.customers.Add(value);
+                dbContext.SaveChanges();
+                Ok(true);
+                return true;
+            }
+            else
+            {
+                NotFound();
+                return false;
+            }
+        }
+
+        // DELETE api/values/5
+        public bool Delete(int id)
+        {
+            customer FoundMatch = this.Get(id);
+
+            if (FoundMatch != null && FoundMatch.id == id)
+            {
+                dbContext.customers.Remove(FoundMatch);
+                dbContext.SaveChanges();
+
+                Ok(true);
+
+                return true;
+            }
+            else
+            {
+                NotFound();
+                return false;
+            }
+        }
+    }
+}
